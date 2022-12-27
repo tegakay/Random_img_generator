7ad7c56175b93d48e644de9f32179eb3704d8c45
Oghenebrume Kofi <kofitega@gmail.com>
2022-12-27 15:12:16 +0100

    temp fix for api down

---
+++ src/components/ErrorComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {BiErrorAlt} from 'react-icons/bi'
+
+
+export const ErrorComponent = () => {
+  return (
+    <>
+    <div>An Error Occured on the API, Try </div>
+    <h1><BiErrorAlt/></h1></>
+  )
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,52 +1,84 @@
 import React, { useState, useEffect } from "react";
 
+
 export default function Main() {
   const [Random, setRandom] = useState();
   const [Loading, setLoading] = useState(true);
-  
+ 
 
   useEffect(() => {
     fetchreq();
   }, []);
 
   const fetchreq = (dy) => {
-    let angle = dy ==="yes"?"&is_nsfw=true":''
+    let angle = dy === "yes" ? "&is_nsfw=true" : "";
+   
     fetch(`https://api.waifu.im/random/?orientation=LANDSCAPE${angle}`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error(res.ok);
+        }
+      })
       .then((data) => setRandom(data))
       .then((data) => {
-        
         return setLoading(false);
-      });
+      })
+      .catch( (error)=>{
+        console.log("code here")
+        return setLoading(false);
+        
+      }
+        
+      )
+        
+      
   };
 
-  function changeimage(check){
-    console.log("rhapsody",check)
-    setLoading(true)
+  function changeimage(check) {
+    setLoading(true);
     fetchreq(check);
   }
   //check if data is in the api
- 
-  let img_name = ""
-    let unloaded = "https://media.tenor.com/NqKNFHSmbssAAAAi/discord-loading-dots-discord-loading.gif";
-    //let loaded =  Random.images[0].url
 
-     img_name = Loading?unloaded:Random.images[0].url;
-     console.log("blue",img_name)
+  let img_name = "";
+  let unloaded =
+    "https://i.gifer.com/1amw.gif";
+  //let loaded =  Random.images[0].url
   
+  var loaded = Random !== undefined?Random.images[0].url : "https://www.pngkey.com/png/detail/147-1473883_404-error-404-not-found-png.png"
+
+ // img_name = Loading ? unloaded : Random.images[0].url;
+
+  img_name = Loading ? unloaded : loaded;
+
+
+  debugger;
+
+  if(!Loading && Random === undefined  ){
+    console.log("here")
+    img_name = "https://www.pngkey.com/png/detail/147-1473883_404-error-404-not-found-png.png"
+  }
 
   return (
     <div className="main">
       <h2>Random Image Of the Day</h2>
-      
+
       <div className="image--wrapper">
-        <img className="image--random" src={img_name} alt="anime"  ></img>
+        <img className="image--random" src={img_name} alt="anime"></img>
       </div>
 
       <div className="img-nav">
-        <button className="button--next" onClick={changeimage}>Random</button>
-        <button className="button--next" onClick={(event)=>changeimage('yes')}>NSFW</button>
-        
+        <button className="button--next" onClick={changeimage}>
+          Random
+        </button>
+        <button
+          className="button--next"
+          onClick={(event) => changeimage("yes")}
+        >
+          NSFW
+        </button>
       </div>
     </div>
   );
